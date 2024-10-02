4261370b173f63a4fa54eb3aeb6415a4c585ae47
Sanjay Choudhary <96792511+cu-sanjay@users.noreply.github.com>
2024-10-02 12:34:16 +0530

    Added automated dependency installation process for easier setup.

---
+++ INSTALL.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Installing required Python libraries...
+
+pip install pyautogui pygetwindow pynput keyboard
+
+echo All required libraries are installed. Run the START Application.
+pause
