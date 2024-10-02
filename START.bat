abf24d673d5286a4a45ce33127363f8de5cc51d9
Sanjay Choudhary <96792511+cu-sanjay@users.noreply.github.com>
2024-10-02 12:36:45 +0530

    Improved user control for a better experience.

---
+++ START.bat
@@ -0,0 +1,17 @@
+@echo off
+echo.
+echo ==============================================
+echo   Welcome to Blum Auto Clicker Script!
+echo   Developed with love by Sannjay.
+echo ----------------------------------------------
+echo.
+
+echo [INFO] Initializing the Clicker... Please wait!
+echo.
+
+set delay=2
+set window_name=1
+
+python blum.py %delay% %window_name%
+
+pause
